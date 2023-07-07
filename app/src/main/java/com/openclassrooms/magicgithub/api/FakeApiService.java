d2d9f35b0ead194773e198d120a8e7b54f345658
Antoine Thomas <antoine@coyali.com>
2023-07-07 12:47:42 +0200

    resolved 2nd to do ready to review

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -2,6 +2,8 @@ package com.openclassrooms.magicgithub.api;
 
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
+import java.util.Random;
+
 import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
 
 public class FakeApiService implements ApiService {
@@ -14,8 +16,8 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+
+        return users;
     }
 
     /**
@@ -24,7 +26,9 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+        // TODO : ici
+        User randomUser = FakeApiServiceGenerator.FAKE_USERS_RANDOM.get(new Random().nextInt(FakeApiServiceGenerator.FAKE_USERS_RANDOM.size()));
+        users.add(randomUser);
     }
 
     /**
@@ -32,6 +36,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
+        // TODO : ici
+        users.remove(user);
     }
 }
